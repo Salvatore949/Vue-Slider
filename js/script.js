59e071a0f547b7326b918a2eb37842c22c9200bd
Salvatore949 <salvo.mess49@gmail.com>
2021-11-11 17:33:59 +0100

    for

--- js/script.js
+++ js/script.js
@@ -1,35 +1,36 @@
-var carousel = new Vue ({
+var carousel = new Vue({
     el:"#container",
     data:{
+        activeSlide: 0,
             photos:[
             {
                 image:'img/01.jpg',
                 tile:'Svezia',
-                text:'Lorem ipsum dolor sit amet'
+                text:'Lorem ipsum dolor sit amet',
             },
 
             {
                 image:'img/02.jpg',
                 tile:'Svizzera',
-                text:'Lorem ipsum dolor sit amet'
+                text:'Lorem ipsum dolor sit amet',
             },
 
             {
                 image:'img/03.jpg',
                 tile:'Gran Bretagna',
-                text:'Lorem ipsum dolor sit amet'
+                text:'Lorem ipsum dolor sit amet',
             },
 
             {
                 image:'img/04.jpg',
                 tile:'Germania',
-                text:'Lorem ipsum dolor sit amet'
+                text:'Lorem ipsum dolor sit amet',
             },
 
             {
                 image:"img/05.jpg",
                 tile:'Olanda',
-                text:'Lorem ipsum dolor sit amet'
+                text:'Lorem ipsum dolor sit amet',
             }
         ],
     }
